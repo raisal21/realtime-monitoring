f332695f06bc99c1dcd77f142d8a8263fdc1cd16
raisal21 <raisal.ahmad.dev@gmail.com>
2026-02-04 20:22:35 +0700

    feat: modification to schema zod

--- src/domain/drilingSchema.ts
+++ src/domain/drilingSchema.ts
@@ -13,3 +13,27 @@ drillingSchema.parse({
     timestapms:1,
     depth:2,
 })
+
+const drillingLog = z.object({
+    timestamp: z.iso.time(),
+    depth: z.number(),
+    pass: z.number(),   
+    direction: z.enum(["up", "down", "steady"])
+})
+
+const chanelState = z.object({
+    mnemonic: z.string(),
+    uom: z.string(),
+    value: z.number()
+})
+
+const depth = z.object({
+    value: z.number(),
+    pass: z.number(),
+    direction: z.enum(["up", "down", "steady"])
+})
+
+const drillingLog2 = z.object({
+    timestamp: z.iso.time(),
+    depth: depth
+})

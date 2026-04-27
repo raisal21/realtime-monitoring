8eba2eec89073cf28e45f37ebed5635a4680dd87
raisal21 <raisal.ahmad.dev@gmail.com>
2026-04-27 19:22:44 +0700

    chore: fix minor routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import DashboardV2 from "./pages/DashboardV2";
 import WellExplorer from "./pages/WellExplorer";
 import Auth from "./pages/Auth";
 
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Navigate to="/wells" replace />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboardv2" element={<DashboardV2 />} />
         <Route path="/wells" element={<WellExplorer />} />
       </Routes>
     </BrowserRouter>
